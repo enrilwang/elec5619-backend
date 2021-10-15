6a85485f880b0fc7a767cbfda4188ee0b9f9b104
en-en-a-a-0v0 <45622952+en-en-a-a-0v0@users.noreply.github.com>
2021-10-15 23:09:24 +1100

    Add files via upload

---
+++ springboot2-webapp-jsp-quickstart (1)/src/main/java/net/guides/springboot2/springboot2webappjsp/repositories/ArtifactRepository.java
@@ -0,0 +1,8 @@
+package net.guides.springboot2.springboot2webappjsp.repositories;
+
+import net.guides.springboot2.springboot2webappjsp.domain.Artifact;
+import net.guides.springboot2.springboot2webappjsp.domain.ArtifactId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArtifactRepository extends JpaRepository<Artifact, ArtifactId> {
+}
