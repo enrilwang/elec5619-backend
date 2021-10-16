6ccc670aad9b049b729232633bdbdde341b44df8
Ynnlink <31876205+Ynnlink@users.noreply.github.com>
2021-10-16 16:07:39 +0800

    Update CategoryRepository.java

--- springboot2-webapp-jsp-quickstart (1)/src/main/java/net/guides/springboot2/springboot2webappjsp/repositories/CategoryRepository.java
+++ springboot2-webapp-jsp-quickstart (1)/src/main/java/net/guides/springboot2/springboot2webappjsp/repositories/CategoryRepository.java
@@ -2,6 +2,14 @@ package net.guides.springboot2.springboot2webappjsp.repositories;
 
 import net.guides.springboot2.springboot2webappjsp.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, String> {
+
+    @Query(value = "select category_name from category", nativeQuery = true)
+    List<String> findAllId();
+
 }
