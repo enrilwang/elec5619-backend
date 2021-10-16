d3de0084dd33b03edf2c40af23c3e4d5e179773d
Ynnlink <31876205+Ynnlink@users.noreply.github.com>
2021-10-16 16:07:18 +0800

    Update ArtifactRepository.java

--- springboot2-webapp-jsp-quickstart (1)/src/main/java/net/guides/springboot2/springboot2webappjsp/repositories/ArtifactRepository.java
+++ springboot2-webapp-jsp-quickstart (1)/src/main/java/net/guides/springboot2/springboot2webappjsp/repositories/ArtifactRepository.java
@@ -1,8 +1,20 @@
-//package net.guides.springboot2.springboot2webappjsp.repositories;
-//
-//import net.guides.springboot2.springboot2webappjsp.domain.Artifact;
-//import net.guides.springboot2.springboot2webappjsp.domain.ArtifactId;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface ArtifactRepository extends JpaRepository<Artifact, ArtifactId> {
-//}
+package net.guides.springboot2.springboot2webappjsp.repositories;
+
+import net.guides.springboot2.springboot2webappjsp.domain.Artifact;
+import net.guides.springboot2.springboot2webappjsp.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ArtifactRepository extends JpaRepository<Artifact, Integer> {
+
+    //public List<Artifact> findByUser_id(Integer user_id);
+
+    @Query(value = "select artifact_id, title, description, category_name, store_location from artifact where user_id = ?1", nativeQuery = true)
+    public List<Map<String,Object>> findByUserId(Integer user_id);
+
+    public Artifact findArtifactByArtifactId(Integer artifact_id);
+
+}
