cc6a68dba6f6508a225ef28c1519faa29de720c1
en-en-a-a-0v0 <45622952+en-en-a-a-0v0@users.noreply.github.com>
2021-10-16 22:04:03 +1100

    Update SubscriptionTypeRepository.java

--- springboot2-webapp-jsp-quickstart (1)/src/main/java/net/guides/springboot2/springboot2webappjsp/repositories/SubscriptionTypeRepository.java
+++ springboot2-webapp-jsp-quickstart (1)/src/main/java/net/guides/springboot2/springboot2webappjsp/repositories/SubscriptionTypeRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SubscriptionTypeRepository extends JpaRepository<SubscriptionType,Integer> {
     SubscriptionType getSubscriptionTypeBySubscriptionTypeId(int subscriptionTypeId);
+    SubscriptionType getSubscriptionTypeByUserId(int userId);
 }
