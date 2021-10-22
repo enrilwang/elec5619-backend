d32300e1c2c9240e0b451607911da5a6c99abdf8
en-en-a-a-0v0 <45622952+en-en-a-a-0v0@users.noreply.github.com>
2021-10-23 00:06:02 +1100

    Mock Repository for Unit Test

--- springboot2-webapp-jsp-quickstart (1)/src/test/java/net/guides/springboot2/springboot2webappjsp/SubscribeControllerTest.java
+++ springboot2-webapp-jsp-quickstart (1)/src/test/java/net/guides/springboot2/springboot2webappjsp/SubscribeControllerTest.java
@@ -1,123 +1,172 @@
 package net.guides.springboot2.springboot2webappjsp;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import net.guides.springboot2.springboot2webappjsp.controllers.Result;
+import lombok.extern.slf4j.Slf4j;
+import net.guides.springboot2.springboot2webappjsp.configuration.JwtInterceptor;
+import net.guides.springboot2.springboot2webappjsp.configuration.JwtUtil;
 import net.guides.springboot2.springboot2webappjsp.controllers.SubscribeController;
 import net.guides.springboot2.springboot2webappjsp.controllers.UserController;
 import net.guides.springboot2.springboot2webappjsp.domain.Subscribe;
+import net.guides.springboot2.springboot2webappjsp.domain.User;
 import net.guides.springboot2.springboot2webappjsp.repositories.SubscribeRepository;
 import net.guides.springboot2.springboot2webappjsp.repositories.UserRepository;
 import org.json.JSONObject;
-import org.junit.Assert;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.MockBeans;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import  org.springframework.web.bind.annotation.*;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-@WebAppConfiguration
-
+@RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
+@WebAppConfiguration
+@Slf4j
+@EnableWebMvc
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@MockBeans({@MockBean(UserRepository.class), @MockBean(SubscribeRepository.class)/*, @MockBean(JwtInterceptor.class)*/})
+@PrepareForTest({JwtUtil.class})
 public class SubscribeControllerTest {
 
+
     @Autowired
-    private MockMvc mvc;
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext wac;
 
     @Autowired
     private UserRepository userRepository;
-
     @Autowired
-    private UserController uc;
-
+    private SubscribeRepository subscribeRepository;
     @Autowired
-    private SubscribeRepository subscribeRepo;
-
+    private SubscribeController subscribeController;
+    @Autowired
+    private UserController userController;
 //    @Autowired
-//    private ToDoService
+//    @Qualifier("jwti")
+//    private JwtInterceptor jwti;
+
+
+//    @MockBean
 
 
     ObjectMapper mapper = new ObjectMapper();
 
     String auth;
-    final String subscribePath = "/subscribe";
+
+    final String registerPath = "/register";
     final String loginPath = "/login";
+    final String subscribePath = "/subscribe";
 
     @Before
-    public void getAuth() throws Exception {
-        Map<String, Object> json = new HashMap<>();
-        json.put("email", "test1@test.com");
-        json.put("password", "enn");
+    public void setup() throws Exception {
+        //Request
+        User userR = new User();
+        userR.setUsername("test");
+        userR.setEmail("test@test.com");
+        //Response
+        User user = new User();
+        user.setEmail("test@test.com");
+        user.setPassword("test");
+        userR.setPassword(DigestUtils.md5DigestAsHex((new String("test")).getBytes()));
+        //Mock DB
+        Mockito.when(this.userRepository.getUserByEmail("test@test.com")).thenReturn(userR);
+        //Mock Request
         RequestBuilder request = MockMvcRequestBuilders.post(loginPath)
-                .content(mapper.writeValueAsString(json))
+                .content(mapper.writeValueAsString(user))
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .accept(MediaType.APPLICATION_JSON);
-        MvcResult response = mvc.perform(request)
-                .andExpect(status().isOk())
+        //Mock API Call
+        MvcResult response = mockMvc.perform(request)
                 .andReturn();
-        JSONObject jsonResponse=new JSONObject(response.getResponse().getContentAsString());
-        try{
-            this.auth=jsonResponse.getString("token");
-        }catch (Exception e){
+
+        JSONObject jsonResponse = new JSONObject(response.getResponse().getContentAsString());
+        try {
+            this.auth = new String(jsonResponse.getString("token"));
+            System.out.println(this.auth);
+        } catch (Exception e) {
+
+        }
+
+    }
+
+
+    //register the first user "test@test.com"
+//    @Test
+    public void test01mockRegister() throws Exception {
+        //Request
+        User userR = new User();
+        userR.setUsername("test");
+        userR.setEmail("test@test.com");
+        //Response
+        User user = new User();
+        user.setEmail("test@test.com");
+        user.setPassword("test");
+        userR.setPassword(DigestUtils.md5DigestAsHex((new String("test")).getBytes()));
+        //Mock DB
+        Mockito.when(this.userRepository.getUserByEmail("test@test.com")).thenReturn(userR);
+        //Mock Request
+        RequestBuilder request = MockMvcRequestBuilders.post(loginPath)
+                .content(mapper.writeValueAsString(user))
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON);
+        //Mock API Call
+        MvcResult response = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+
+        JSONObject jsonResponse = new JSONObject(response.getResponse().getContentAsString());
+        try {
+            this.auth = new String(jsonResponse.getString("token"));
+            System.out.println(this.auth);
+        } catch (Exception e) {
 
         }
     }
 
     @Test
-    public void testCheckBalance1() throws Exception {
+    public void test03testCheckBalance1() throws Exception {
         System.out.println(this.auth);
-        Subscribe json =new Subscribe();
-        json.setCreatorId(-1);
-        json.setSubscribeId(-1);
-        json.setSubscribeType(-1);
-//        Mockito.when(subscribeRepo.save(json)).thenReturn(json);
+        Subscribe subscribe = new Subscribe();
+        subscribe.setCreatorId(-1);
+        subscribe.setSubscribeId(-1);
+        subscribe.setSubscribeType(-1);
+        Mockito.when(subscribeRepository.save(subscribe)).thenReturn(subscribe);
         RequestBuilder request = MockMvcRequestBuilders.post(subscribePath)
-                .content(mapper.writeValueAsString(json))
-                .header("Authorization",this.auth)
-                .contentType(MediaType.APPLICATION_JSON_UTF8);
-        MvcResult response = mvc.perform(request)
+                .content(mapper.writeValueAsString(subscribe))
+                .header("Authorization", this.auth)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON);
+        MvcResult response = mockMvc.perform(request)
                 .andExpect(status().is4xxClientError())
+                .andDo(print())
                 .andReturn();
-//        String responseString =response.getResponse().getContentAsString();
-//        JSONObject jsonResponse=null;
-//        try{
-//            jsonResponse=new JSONObject(responseString);
-//        }catch (Exception e){
+
+
 //
-//        }
-
-//        if (!responseString.isEmpty()){
-//            jsonResponse=new JSONObject(responseString);
-//        }
-//        System.out.println(jsonResponse);
-//        System.out.println("==");
-//        System.out.println(response.getResponse().getContentAsString());
-//        System.out.println(new JSONObject(responseString));
-//        Assert.assertEquals("1", jsonResponse.getString("code"));
-//        Assert.assertEquals("Creator is not existed", jsonResponse.getString("msg"));
-//        Assert.assertEquals("Required int parameter 'month' is not present",jsonResponse.getString("msg"));
-//        Assert.assertEquals("[]", jsonResponse.getString("data"));
-
-
 
 
 //        var authentication = HttpContext.GetOwinContext().Authentication;
